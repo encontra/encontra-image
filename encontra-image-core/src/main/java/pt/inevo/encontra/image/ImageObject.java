421e548578946949612dc8aa79f01a7763117493
Ricardo Dias <rspd.sao.pedro@gmail.com>
2010-07-06 17:56:53 +0100

    * Change: changes in the index and abstractobject.

--- encontra-image-core/src/main/java/pt/inevo/encontra/image/ImageObject.java
+++ encontra-image-core/src/main/java/pt/inevo/encontra/image/ImageObject.java
@@ -10,11 +10,17 @@ import pt.inevo.encontra.index.AbstractObject;
  * Image Object for the EnConTRA API
  * @author ricardo
  */
+<<<<<<< HEAD:encontra-lucene-engine/src/main/java/pt/inevo/encontra/lucene/ImageObject.java
+public class ImageObject extends AbstractObject<String,BufferedImage> {
+
+    public ImageObject(BufferedImage image, String identifier){
+=======
 public class ImageObject<I> extends AbstractObject<I,BufferedImage> {
     public ImageObject() {
         super();
     }
     public ImageObject(I identifier,BufferedImage image){
+>>>>>>> 545adf864a876185843fe635cde7171cd5d26d22:encontra-image/encontra-image-core/src/main/java/pt/inevo/encontra/image/ImageObject.java
         super(identifier, image);
     }
 
