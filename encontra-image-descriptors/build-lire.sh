f04a0a40ba3b838cc1a61d0d57be93df44e4eccb
nelson.silva <nelson.silva@inevo.pt>
2011-11-09 10:24:37 +0000

    Started work on JS descriptors

--- encontra-image-descriptors/build-lire.sh
+++ encontra-image-descriptors/build-lire.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-svn co https://caliph-emir.svn.sourceforge.net/svnroot/caliph-emir/lire
+svn co https://caliph-emir.svn.sourceforge.net/svnroot/caliph-emir/lire -r 69
+svn co https://caliph-emir.svn.sourceforge.net/svnroot/caliph-emir/caliphemir/src -r 30 caliphemir-src
 ant -f build-lire.xml install
